892af39a08138019c8fc2d188ff25b240af94a65
aswad <aswad.m.tariq13@gmail.com>
2026-01-18 23:10:48 +0800

    add input modal and options

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,27 @@
-import Button from '@/components/Button';
-import { router } from 'expo-router';
-import { StyleSheet, View } from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import Button from "@/components/Button";
+import { router } from "expo-router";
+import { useState } from "react";
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const [addModalVisible, setAddModalVisible] = useState(false);
 
   const handleAddNewVisit = () => {
-    router.push('../pages/addVisitPage');
+    setAddModalVisible(true);
+  };
+
+  const handleInputVisitForm = () => {
+    setAddModalVisible(false);
+    router.push("../pages/addVisitPage");
+  };
+
+  const closeModal = () => {
+    setAddModalVisible(false);
   };
 
   const handleSeePastVisit = () => {
-    router.push('../pages/pastVisitsPage');
+    router.push("../pages/pastVisitsPage");
   };
 
   return (
@@ -21,18 +32,42 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <View style={{ flex: 1, gap: 15, justifyContent: 'center', alignItems: 'center' }}>
-        <Button 
-          label="Add visit record" 
-          onPress={handleAddNewVisit}
-          style={styles.button}  
+      <View
+        style={{
+          flex: 1,
+          gap: 15,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          label="Add visit record"
+          onPress={() => setAddModalVisible(true)}
+          style={styles.button}
         />
-        <Button 
-          label="See past visits" 
+        <Button
+          label="See past visits"
           onPress={handleSeePastVisit}
-          style={styles.button}  
+          style={styles.button}
         />
       </View>
+      <Modal visible={addModalVisible} animationType="fade" transparent={true}>
+        <TouchableOpacity
+          style={styles.modalContainer}
+          onPress={() => setAddModalVisible(false)}
+        >
+          <View style={styles.modalCard}>
+            <Text style={styles.modalTitle}>Add Visit Record</Text>
+            <Button label="Scan Visit" style={styles.buttonModal} />
+            <Button
+              label="Input Visit Form"
+              style={styles.buttonModal}
+              onPress={handleInputVisitForm}
+            />
+            <Button label="Import Visit" style={styles.buttonModal} />
+          </View>
+        </TouchableOpacity>
+      </Modal>
     </SafeAreaView>
   );
 }
@@ -41,6 +76,34 @@ const styles = StyleSheet.create({
   button: {
     borderWidth: 1,
     borderRadius: 18,
-    borderColor: '#007AFF',
+    borderColor: "#007AFF",
+  },
+  buttonModal: {
+    borderWidth: 1,
+    borderRadius: 18,
+    borderColor: "#007AFF",
+    width: "90%",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalCard: {
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "#007AFF",
+    borderRadius: 18,
+    width: "80%",
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 12,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
 });
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,40 +1,47 @@
-import { Pressable, StyleProp, StyleSheet, Text, View, ViewStyle } from 'react-native';
+import {
+    Pressable,
+    StyleProp,
+    StyleSheet,
+    Text,
+    View,
+    ViewStyle,
+} from "react-native";
 
 type Props = {
-    label: string;
-    onPress?: () => void;
-    style?: StyleProp<ViewStyle>;
+  label: string;
+  onPress?: () => void;
+  style?: StyleProp<ViewStyle>;
 };
 
 export default function Button({ label, onPress, style }: Props) {
-    return (
-        <View style={[styles.buttonContainer, style]} >
-            <Pressable onPress={onPress} style={[styles.button]} >
-                <Text style={styles.buttonLabel}>{label}</Text>
-            </Pressable>
-        </View>
-    );
+  return (
+    <View style={[styles.buttonContainer, style]}>
+      <Pressable onPress={onPress} style={[styles.button]}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        width: 320,
-        height: 68,
-        marginHorizontal: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 3,
-    },
-    button: {
-        borderRadius: 10,
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-    },
-    buttonLabel: {
-        color: '#000000ff',
-        fontSize: 16,
-    },
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  buttonLabel: {
+    color: "#000000ff",
+    fontSize: 16,
+  },
 });
